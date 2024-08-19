2d5bf175799055e90a7a27b4976fdecf8c5428f8
rtbf <popovrromma@gmail.com>
2024-08-19 16:47:59 +1000

    add filter ID

--- personal/filters.py
+++ personal/filters.py
@@ -8,6 +8,7 @@ class CarFilter(django_filters.FilterSet):
     max_mileage = django_filters.NumberFilter(field_name="mileage", lookup_expr='lte')
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
+    id = django_filters.NumberFilter(field_name="id")
     class Meta:
         model = Car
         fields = '__all__'
