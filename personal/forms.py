b2f4086b554406fd9bc58e690560c27d2f282fa3
rtbf <popovrromma@gmail.com>
2024-08-19 12:27:23 +1000

    add  dropdown list

--- personal/forms.py
+++ personal/forms.py
@@ -1,9 +1,12 @@
+from django import forms
 from django.forms import ModelForm
-
 from .models import Car
 
 class AddForm(ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
-        
+        widgets = {
+            'type_fuel': forms.Select(),
+            'type_KPP': forms.Select(),
+        }
--- personal/models.py
+++ personal/models.py
@@ -4,14 +4,28 @@ from django.db import models
 
 
 class Car(models.Model):
+    FUEL_CHOICES = [
+        ('petrol', 'Petrol'),
+        ('diesel', 'Diesel'),
+        ('electric', 'Electric'),
+        ('hybrid', 'Hybrid'),
+    ]
+    
+    KPP_CHOICES = [
+        ('mechanical', 'Mechanical'),
+        ('automatic', 'Automatic'),
+        ('variator', 'Variator'),
+        ('robot', 'Robot'),
+
+    ]
+
     marka = models.CharField(max_length=40)
     model = models.CharField(max_length=40)
     year = models.IntegerField()
-    type_fuel = models.CharField(max_length=13)
-    type_KPP = models.CharField(max_length=14)
+    type_fuel = models.CharField(max_length=13, choices=FUEL_CHOICES)
+    type_KPP = models.CharField(max_length=14, choices=KPP_CHOICES)
     mileage = models.IntegerField()
     price = models.IntegerField()
 
     def __str__(self):
         return self.marka
-
